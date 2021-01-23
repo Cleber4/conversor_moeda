b2e5c27630159154dbd473e171681ff63665033b
Cleberson Gilvan <cleber.gilvan@gmail.com>
2021-01-23 20:23:18 -0300

    [#01] - home view

---
+++ lib/app/models/currency_model.dart
@@ -0,0 +1,9 @@
+class CurrencyModel {
+  final String name;
+  final double real;
+  final double dollar;
+  final double euro;
+  final double bitcoin;
+
+  CurrencyModel({this.name, this.real, this.dollar, this.euro, this.bitcoin});
+}
